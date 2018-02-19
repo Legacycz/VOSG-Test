67778bf8270e083758a376edd4e8d168c2d89dd1
SUSG-JI\dolezal <snake2112@seznam.cz>
2018-02-19 14:40:58 +0100

    Some stuff...

--- Assets/Milan.cs
+++ Assets/Milan.cs
@@ -1,13 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Milan : MonoBehaviour {
+public class Milan : MonoBehaviour
+{
+    private int myIterator;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         print("Hello, I'm Terka");
-	}
+
+        for (int i = 0; i < 10; ++i)
+        {
+            print(i);
+        }
+
+
+
+        if(myIterator == 1)
+        {
+
+        }
+        else if(myIterator == 2)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
---
+++ Assets/PlatformText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlatformText : MonoBehaviour {
+    [SerializeField]
+    private Text buttonText;
+
+	// Use this for initialization
+	void Start ()
+    {
+#if UNITY_STANDALONE
+        buttonText.text = "Standalone";
+#elif UNITY_WEBGL
+        buttonText.text = "WebGL";
+#elif UNITY_ANDROID
+        buttonText.text = "Android";
+#endif
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
