e952d8b417f8e9260d8ff9afeba3e31ba417bc92
SUSG-JI\dolezal <snake2112@seznam.cz>
2018-02-19 13:24:01 +0100

    Test commit

--- Assets/Milan1.cs
+++ Assets/Milan1.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Milan : MonoBehaviour {
+public class Milan1 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        print("Hello, I'm Terka");
+		
 	}
 	
 	// Update is called once per frame
